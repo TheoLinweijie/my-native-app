47d6b95619632866836341dabafa296c4e962f57
TheoLinweijie <49381830+TheoLinweijie@users.noreply.github.com>
2020-03-26 13:12:50 +0800

    First Commit

---
+++ navigation/BottomTabNavigator.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import PopularScreen from '../screens/PopularScreen';
+
+const BottomTab = createBottomTabNavigator();
+const INITIAL_ROUTE_NAME = 'Home';
+
+export default function BottomTabNavigator({ navigation, route }) {
+  // Set the header title on the parent stack navigator depending on the
+  // currently active tab. Learn more in the documentation:
+  // https://reactnavigation.org/docs/en/screen-options-resolution.html
+  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+
+  return (
+    <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Trending',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-home" />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Homes"
+        component={PopularScreen}
+        options={{
+          title: 'Popular',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-star" />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Links"
+        component={LinksScreen}
+        options={{
+          title: 'Resources',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-search" />,
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+function getHeaderTitle(route) {
+  const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+
+  switch (routeName) {
+    case 'Home':
+      return 'Trending Moive';
+    case 'Homes':
+      return 'Popular Movie';
+    case 'Links':
+      return 'Search';
+  }
+}
